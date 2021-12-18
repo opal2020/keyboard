9d7122dbbb98dd80d89e73892a1008d355e76a40
Lee Ji Min <ljm2020@hanyang.ac.kr>
2021-12-19 03:49:02 +0900

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 module.exports = {
+  assetPrefix:
+    process.env.NODE_ENV === 'production'
+      ? 'https://opal2020.github.io/keyboard'
+      : '',
   images: {
-    loader: 'imgix',
-    path: 'https://noop/',
+    loader: 'akamai',
+    path:
+      process.env.NODE_ENV === 'production'
+        ? 'https://opal2020.github.io/keyboard'
+        : '',
   },
 };
